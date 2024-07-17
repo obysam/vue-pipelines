975c73e391e248c5dfbadf9dc8b617e65909d994
LAPTOP-Q3FLM4T3\magta <oby.samuel@gmail.com>
2024-07-17 15:58:11 +0700

    push update

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,17 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  publicPath: process.env.NODE_ENV === "production" ? "/vue-pipelines/" : "/",
+  configureWebpack:{
+    performance: {
+      hints: false
+    },
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      }
+    }
+  }
 })
