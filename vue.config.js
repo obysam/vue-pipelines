765b24ed263ca6aeb068f8641972b7ea637a393b
LAPTOP-Q3FLM4T3\magta <oby.samuel@gmail.com>
2024-07-17 16:07:05 +0700

    push

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ module.exports = defineConfig({
   transpileDependencies: [
     'vuetify'
   ],
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-pipelines/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/",
   configureWebpack:{
     performance: {
       hints: false
